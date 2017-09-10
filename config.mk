8af4260e570503ee23080d079d710f22efb1d998
Pranav Vashi <neobuddy89@gmail.com>
2017-09-10 20:29:05 +0530

    addons: Copy lib for swype
    * Debug the crash. Don't hide the scars.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -22,8 +22,8 @@ PRODUCT_PACKAGES += \
     RedOverlay \
     Turbo
 
-#PRODUCT_COPY_FILES += \
-#    vendor/addons/prebuilt/system/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+PRODUCT_COPY_FILES += \
+    vendor/addons/prebuilt/system/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
 # Camera Effects for devices without a vendor partition
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
