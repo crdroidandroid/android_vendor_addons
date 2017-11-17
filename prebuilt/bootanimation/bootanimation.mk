f02944790024e6b65e0e1691f411cbe261901888
Pranav Vashi <neobuddy89@gmail.com>
2017-11-17 23:21:02 +0530

    addons: Change default bootanimation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- prebuilt/bootanimation/bootanimation.mk
+++ prebuilt/bootanimation/bootanimation.mk
@@ -12,36 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-# determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# determine the bigger dimension
-TARGET_BOOTANIMATION_SIZE_ALT := $(shell \
-  if [ "$(TARGET_SCREEN_WIDTH)" -gt "$(TARGET_SCREEN_HEIGHT)" ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# first try matching and use smaller dimension bootanimation
-ifneq ($(filter 480 720 1080 1440,$(TARGET_BOOTANIMATION_SIZE)),)
-PRODUCT_BOOTANIMATION := vendor/addons/prebuilt/bootanimation/$(TARGET_BOOTANIMATION_SIZE).zip
-else
-# if not try matching and use bigger dimension bootanimation
-ifneq ($(filter 480 720 1080 1440,$(TARGET_BOOTANIMATION_SIZE_ALT)),)
-PRODUCT_BOOTANIMATION := vendor/addons/prebuilt/bootanimation/$(TARGET_BOOTANIMATION_SIZE_ALT).zip
-else
-# if not found use default bootanimation
 PRODUCT_BOOTANIMATION := vendor/addons/prebuilt/bootanimation/bootanimation.zip
-endif
-endif
-
-else
-PRODUCT_BOOTANIMATION := vendor/addons/prebuilt/bootanimation/bootanimation.zip
-endif
