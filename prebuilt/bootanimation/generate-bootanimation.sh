2bee48e0949267378abeeda2827f7d577a93e108
Pranav Vashi <neobuddy89@gmail.com>
2018-09-24 00:53:02 +0530

    addons: Support more resolutions for boot animation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- prebuilt/bootanimation/generate-bootanimation.sh
+++ prebuilt/bootanimation/generate-bootanimation.sh
@@ -25,14 +25,25 @@ fi
 IMAGEHEIGHT=$(expr $IMAGEWIDTH \* 16 / 9)
 RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
 
+rm -rf $OUT >> null
+
 for part_cnt in 0 1 2 3
 do
-    mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
+    mkdir -p $OUT/bootanimation/part$part_cnt
 done
-tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/"
+
+if [ "$IMAGESCALEWIDTH" -ge 1440 ]; then
+    tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation_1440.tar" -C "$OUT/bootanimation/"
+elif [ "$IMAGESCALEWIDTH" -ge 1080 ]; then
+    tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation_1080.tar" -C "$OUT/bootanimation/"
+else
+    tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation_720.tar" -C "$OUT/bootanimation/"
+fi
+
 #mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
 
 # Create desc.txt
+touch $OUT/bootanimation/desc.txt
 echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT $BOOTFPS" > "$OUT/bootanimation/desc.txt"
 cat "vendor/addons/prebuilt/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
@@ -40,3 +51,4 @@ cat "vendor/addons/prebuilt/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.
 cd "$OUT/bootanimation"
 
 zip -qr0 "$OUT/bootanimation.zip" .
+
