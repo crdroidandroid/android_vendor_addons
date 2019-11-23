512334cceb526fec131f8e1b9ffc96c2229825e9
Pranav Vashi <neobuddy89@gmail.com>
2019-11-23 23:33:04 +0530

    addons: Redo for compatibility with ThemePicker
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- themes/accents/deepOrange/Android.mk
+++ themes/accents/deepOrange/Android.mk
@@ -4,8 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := DeepOrange
+LOCAL_RRO_THEME := DeepOrange
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
 LOCAL_PRIVILEGED_MODULE := false
 
-include $(BUILD_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
