9dccdf1ad53d21a4a08d4fd68dd84aa5b7c232a8
Pranav Vashi <neobuddy89@gmail.com>
2022-02-20 02:07:09 +0530

    addons: Add survival script for search
    * Maybe add GMS build check?
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -36,6 +36,9 @@ PRODUCT_COPY_FILES += \
     vendor/addons/prebuilt/product/etc/sysconfig/nexus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/nexus.xml \
     vendor/addons/prebuilt/product/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
+PRODUCT_COPY_FILES += \
+    vendor/addons/prebuilt/system/addon.d/70-velvet.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/70-velvet.sh
+
 # Turbo Prebuilt package for Battery Estimates
 PRODUCT_PACKAGES += \
     TurboPrebuilt
