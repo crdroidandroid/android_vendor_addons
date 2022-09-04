4623c52736abfde56b75ca4fb37428b874278d7d
Pranav Vashi <neobuddy89@gmail.com>
2022-09-04 19:11:34 +0530

    addons: Remove google prebuilts for now
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -17,42 +17,6 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 PRODUCT_PACKAGE_OVERLAYS += vendor/addons/overlay/common
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/addons/overlay/common
 
-ifeq ($(TARGET_HAS_UDFPS),true)
-PRODUCT_PACKAGES += \
-    UdfpsIcons \
-    UdfpsAnimations
-endif
-
-PRODUCT_COPY_FILES += \
-    vendor/addons/prebuilt/product/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/dialer_experience.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google_build.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_build.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google_exclusives_enable.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_exclusives_enable.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/google-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/nexus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/nexus.xml \
-    vendor/addons/prebuilt/product/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
-
-ifneq ($(WITH_GMS),true)
-PRODUCT_COPY_FILES += \
-    vendor/addons/prebuilt/system/addon.d/70-velvet.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/70-velvet.sh \
-    $(call find-copy-subdir-files,*,vendor/addons/prebuilt/product/usr/,$(TARGET_COPY_OUT_PRODUCT)/usr)
-endif
-
-# Prebuilt packages for device features
-PRODUCT_PACKAGES += \
-    DeviceIntelligenceNetworkPrebuilt \
-    DevicePersonalizationPrebuiltPixel2021 \
-    SettingsIntelligenceGooglePrebuilt \
-    TurboPrebuilt
-
-ifneq ($(TARGET_DOESNT_LIKE_FLIPENDO),true)
-PRODUCT_PACKAGES += \
-    FlipendoPrebuilt
-
-BOARD_SEPOLICY_DIRS += \
-    hardware/google/pixel-sepolicy/flipendo
-endif
-
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
