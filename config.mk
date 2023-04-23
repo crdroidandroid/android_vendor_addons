56f8ea700c43ea3b1826899b872f12b35831dbef
SamarV-121 <samarvispute121@pm.me>
2023-04-23 17:07:35 +0530

    themes: overlays: Add brightness slider styles [3/3]
    Change-Id: I5ec57a37727a1d543bc102b84f1d8cfc2d85af3e

--- config.mk
+++ config.mk
@@ -272,6 +272,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OmniJaws
 
+# Brightness slider styles
+PRODUCT_PACKAGES += \
+    BrightnessSliderFilledOverlay \
+    BrightnessSliderThinOverlay
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
