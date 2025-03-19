f6dbb07993912dbd8f50b20f2194d910ba39a003
Pranav Vashi <neobuddy89@gmail.com>
2025-03-20 02:46:52 +0530

    addons: Remove gestural navigation overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -260,14 +260,6 @@ PRODUCT_PACKAGES += \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay
 
-# Navbar
-PRODUCT_PACKAGES += \
-    GesturalNavigationNarrowSpace \
-    GesturalNavigationNoHint \
-    GesturalNavigationNoSpace \
-    GesturalNavigationOverlayLong \
-    GesturalNavigationOverlayMedium
-
 # Navbar styles
 PRODUCT_PACKAGES += \
     NavbarAndroidOverlay \
